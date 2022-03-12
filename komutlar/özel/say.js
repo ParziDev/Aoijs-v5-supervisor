0b1a11b72656bd58f380accc71fb9fdee4f94a64
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-03-12 09:45:34 +0300

    Create say.js

---
+++ komutlar/özel/say.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name:"say",
+  code:`
+  $description[1;
+\`•\` Sunucumuzda toplam **$membersCount** üye bulunmakta
+\`•\` Sunucumuzda toplam **$math[$membersCount-$membersCount[$guildID;offline]]** aktif üye bulunmakta
+\`•\` Sunucumuzda toplam **$roleMembersCount[$getServerVar[tagrol]]** taglı üyemiz bulunmakta
+\`•\` Sunucumuza toplam **$serverBoostCount** boost basılmış]
+$footer[1;Developed by ParzivâL]
+$color[1;010307]  
+$onlyPerms[admin;]
+  `
+  }
