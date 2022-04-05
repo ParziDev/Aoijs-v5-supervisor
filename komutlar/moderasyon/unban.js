d9f6ebed9aee4552624d89c7eee2ea33793d750e
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-04-05 09:15:03 +0300

    Update unban.js

--- komutlar/moderasyon/unban.js
+++ komutlar/moderasyon/unban.js
@@ -1,7 +1,7 @@
 module.exports = {
   name:"unban",
   code:`
-  $channelSendMessage[$getServerVar[cezalog];{newEmbed:{description:Bir üyenin banı açıldı!}{field:Kullanıcı:$userTag[$message[1]] - \`($message[1])\`}{field:Yetkili:<@$authorID> - \`($authorID)\`}{color:010307}{footer:Developed by ParzivâL}{thumbnail:$userAvatar[$message[1]]}}]
+  $channelSendMessage[$getServerVar[cezalog];{newEmbed:{author:Bir üyenin banı açıldı!}{field:Kullanıcı:$userTag[$message[1]] - \`($message[1])\`}{field:Yetkili:<@$authorID> - \`($authorID)\`}{color:010307}{footer:Developed by ParzivâL}{thumbnail:$userAvatar[$message[1]]}}]
   $channelSendMessage[$channelID;{newEmbed:{author:Üyenin banı açıldı!}{description:$getServerVar[tick] \`$userTag[$message[1]]\` adlı kullanıcının banı açıldı.}{color:010307}{footer:Developed by ParzivâL}{thumbnail:$userAvatar[$message[1]]}]
   $unban[$message[1]]
   $onlyIf[$isBanned[$message[1]]==true;$getServerVar[cross] <@$authorID>, Bu kullanıcı zaten banlanmamış.]
