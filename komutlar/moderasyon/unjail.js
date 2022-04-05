2af549952a4ce464cf3a714b0058124ccd833195
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-04-05 09:15:37 +0300

    Update unjail.js

--- komutlar/moderasyon/unjail.js
+++ komutlar/moderasyon/unjail.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "unjail",
   code: `
-  $channelSendMessage[$getServerVar[cezalog];{newEmbed:{description:Bir üye jailden çıkarıldı!}{field:Kullanıcı:$userTag[$message[1]] - \`($message[1])\`}{field:Yetkili:<@$authorID> - \`($authorID)\`}{color:010307}{footer:Developed by ParzivâL}{thumbnail:$userAvatar[$message[1]]}}]
+  $channelSendMessage[$getServerVar[cezalog];{newEmbed:{author:Bir üye jailden çıkarıldı!}{field:Kullanıcı:$userTag[$message[1]] - \`($message[1])\`}{field:Yetkili:<@$authorID> - \`($authorID)\`}{color:010307}{footer:Developed by ParzivâL}{thumbnail:$userAvatar[$message[1]]}}]
   $channelSendMessage[$channelID;{newEmbed:{author:Üye jailden çıkarıldı!}{description:$getServerVar[tick] \`$userTag[$message[1]]\` adlı üye jailden çıkarıldı.}{color:010307}{footer:Developed by ParzivâL}{thumbnail:$userAvatar[$message[1]]}}]
   $setUserVar[jail;no;$message[1]]
   $giveRole[$guildID;$message[1];$replaceText[$replaceText[$replaceText[$getUserVar[cinsiyet;$message[1]];kayıtsız;$getServerVar[kayıtsız]];erkek;$getServerVar[erkek]];kız;$getServerVar[kız]]]
