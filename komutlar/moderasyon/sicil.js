11c652fceb4ea24d1bef3a2cdbf3dd1c45dc9ff5
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-04-05 08:52:18 +0300

    Update sicil.js

--- komutlar/moderasyon/sicil.js
+++ komutlar/moderasyon/sicil.js
@@ -1,10 +1,15 @@
 module.exports = {
   name:"sicil",
   code:` 
-  $createFile[$userTag[$mentioned[1]] sicili
+  $author[1;$userTag[$message[1]];$userAvatar[$message[1]]]
+  $description[1;Kullanıcının sicili
 
-$getUserVar[sicil;$mentioned[1]]
-
-Developed by ParzivâL;sicil.txt]
+$getUserVar[sicil;$message[1]]]
+  $color[1;010307]
+  $thumbnail[1;$userAvatar[$message[1]]
+  $footer[1;Developed by ParzivâL]
+  $onlyIf[$userExists[$message[1]]==true;$getServerVar[cross] <@$authorID>, Böyle bir kullanıcı yok.]
+  $onlyIf[$message[1]!=;$getServerVar[cross] <@$authorID>, Kullanıcı ID'si gir.]
+  $onlyForRoles[$getServerVar[registerhammer];$getServerVar[cross] <@$authorID>, Bunu kullanamazsın.]
   `
   }
