a34625a7f242a502f1e3eee92d64e83cfa8a7fef
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-04-05 08:43:59 +0300

    Update ban.js

--- komutlar/moderasyon/ban.js
+++ komutlar/moderasyon/ban.js
@@ -3,7 +3,7 @@ module.exports = [{
   aliases:"yasakla",
   code:`
   $sendDM[**$serverName** sunucusundan *$messageSlice[1]* sebebinden banlandın.;$message[1]]
-  $setUserVar[sicil;$getUserVar[sicil;$message[1]]\n$userTag tarafından \`$messageSlice[2]\` sebebinden mutelenmiş.;$message[1]]
+  $setUserVar[sicil;$getUserVar[sicil;$message[1]]\n**[ BAN ]** \`$messageSlice[1]\` ( <@$authorID> );$message[1]]
   $channelSendMessage[$getServerVar[cezalog];{newEmbed:{author:Bir üye banlandı:$userAvatar[$message[1]]}{field:Kullanıcı:$userTag[$message[1]] - \`($message[1])\`}{field:Sebep:$messageSlice[1]}{field:Yetkili:<@$authorID> - \`($authorID)\`}{footer:Developed by ParzivâL}{color:010307}{thumbnail:$userAvatar[$message[1]]}}]
   $channelSendMessage[$channelID;{newEmbed:{author:Üye banlandı!}{description:$getServerVar[tick] \`$userTag[$message[1]]\` adlı kullanıcı **$messageSlice[1]** sebebinden banlandı.}{color:010307}{footer:Developed by ParzivâL}{thumbnail:$userAvatar[$message[1]]}}]
   $ban[$message[1];$guildID;0;$messageSlice[1]]
@@ -22,6 +22,7 @@ module.exports = [{
     type:"awaited",
     code:`
   $sendDM[**$serverName** sunucusundan **$messageSlice[1]** sebebinden banlandın.;$message[1]]
+  $setUserVar[sicil;$getUserVar[sicil;$message[1]]\n**[ BAN ]** \`$messageSlice[1]\` ( <@$authorID> );$message[1]]
   $channelSendMessage[$getServerVar[cezalog];{newEmbed:{description:Bir üye banlandı}{field:Kullanıcı:$userTag[$message[1]] - \`($message[1])\`}{field:Sebep:$messageSlice[1]}{field:Yetkili:<@$authorID> - \`($authorID)\`}{footer:Developed by ParzivâL}{color:010307}{thumbnail:$userAvatar[$message[1]]}}]
   $channelSendMessage[$channelID;{newEmbed:{author:Üye banlandı!}{description:$getServerVar[tick] \`$userTag[$message[1]]\` adlı kullanıcı *__$messageSlice[1]__* sebebinden banlandı.}{color:010307}{footer:Developed by ParzivâL}{thumbnail:$userAvatar[$message[1]]}}]
   $ban[$message[1];$guildID;0;$messageSlice[1]]
