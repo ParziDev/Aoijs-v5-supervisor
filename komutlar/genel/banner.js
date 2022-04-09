b712e2d634aa022741089c7540b14ae82a3882cd
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-04-09 13:34:58 +0300

    Create banner.js

---
+++ komutlar/genel/banner.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name:"banner",
+  aliases:["thumb","thumbnail"],
+  code:`
+  $image[1;$getUserBanner[$mentioned[1]]
+  $color[1;ff0000]
+  $footer[1;Developed by ParzivâL]
+  `
+  }
