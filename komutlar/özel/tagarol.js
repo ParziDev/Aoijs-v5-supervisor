143c4a5960af05765b8f433e561dbf37760a989b
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-04-09 13:31:51 +0300

    Create tagarol.js

---
+++ komutlar/özel/tagarol.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name:"tagrol",
+  code:`
+  $forEachMember[1;{};tagrol2;]
+  $forEachMember[1;{};tagrol1;]
+  $getServerVar[tick] <@$authorID>, Tagı olup rolü olmayan üyelere tagrolü verildi.
+  $onlyPerms[admin;]
+`
+  }
